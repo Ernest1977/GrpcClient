fec66b66cd3596bf7120d35fd3a1c152db156cf0
HP <HP@DESKTOP-E39375N>
2025-01-11 13:00:50 +0100

    Aggiungere i file di progetto.

---
+++ GrpcClient/DiscountClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Stripe;
+
+namespace GrpcClient
+{
+    //internal class DiscountClient
+    //{
+        public class DiscountClient
+            {
+                private class GenerateRequest {
+                    public int Count { get; set; }
+                }
+
+                private class UseRequest
+                {
+                    public int Count { get; set; }
+                    public string Code { get; internal set; }
+                }
+
+
+                private readonly Discount.DiscountClient _client;
+
+                public DiscountClient(string address)
+                {
+                    var channel = GrpcChannel.ForAddress(address);
+                    _client = new Discount.DiscountClient(channel);
+                }
+
+                public async Task GenerateDiscountCodesAsync(int count)
+                {
+                    var request = new GenerateRequest { Count = count };
+                    var response = await _client.GenerateDiscountCodesAsync(request);
+
+                    Console.WriteLine("Generated Discount Codes:");
+                    foreach (var code in response.Codes)
+                    {
+                        Console.WriteLine(code);
+                    }
+                }
+
+                public async Task UseDiscountCodeAsync(string code)
+                {
+                    var request = new UseRequest { Code = code };
+                    var response = await _client.UseDiscountCodeAsync(request);
+
+                    Console.WriteLine(response.Success ? "Code used successfully" : "Code already used");
+                }
+            }
+    //}
+}
---
+++ GrpcClient/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Threading.Tasks;
+using GrpcClient;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new DiscountClient("https://localhost:5001");
+
+        await client.GenerateDiscountCodesAsync(10);
+        await client.UseDiscountCodeAsync("ABC12345");
+    }
+}
